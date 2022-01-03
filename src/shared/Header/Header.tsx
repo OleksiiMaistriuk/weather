0d3dc27ec31ec52d75cbf4ae6c1130b5ab2acc85
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2022-01-03 19:42:38 +0100

    add styles for header

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { GlobalSvgSelector } from '../../assets/icons/global/GlobalSvgSelector';
+import Select from 'react-select';
 import s from './Header.module.scss';
 
 interface Props {}
 export const Header = (props: Props) => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
   return (
     <header className={s.header}>
-      <div className={s.wrapper}>
-        <svg className={s.logo}>
+      <div className={s.wraper}>
+        <div className={s.logo}>
           <GlobalSvgSelector id="header-logo" />
-        </svg>
+        </div>
         <h1 className={s.title}>Reacr Weather</h1>
       </div>
-      <div className={s.change_theme}>
-        <svg className={s.logo}>
+      <div className={s.wraper}>
+        <div className={s.change_theme}>
           <GlobalSvgSelector id="change-theme" />
-        </svg>
+        </div>
+        <Select options={options} />
       </div>
     </header>
   );
