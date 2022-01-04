c5d342b080f321edec274df8b0cc0b67d4331f3f
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2022-01-04 19:10:17 +0100

    add ThisDay styles

--- src/pages/Home/components/ThisDay/ThisDay.tsx
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector';
 import s from './ThisDay.module.scss';
 interface Props {}
 
 export const ThisDay = (props: Props) => {
   return (
     <section className={s.this__day}>
-      <ul className={s.top__block}>
-        <li className={s.this__temp}>20</li>
-        <li className={s.this__day_day}>today</li>
-      </ul>
-      <ul className={s.bottom__block}>
-        <li className={s.this__time}>
-          <span>21:50</span>
-        </li>
-        <li className={s.this__city}>New York</li>
-      </ul>
+      <div className={s.top__block}>
+        <ul className={s.top__block_wrapper}>
+          <li className={s.this__temp}>20</li>
+          <li className={s.this__day_name}>today</li>
+        </ul>
+
+        <GlobalSvgSelector id="sun" />
+      </div>
+      <div className={s.bottom__block}>
+        <ul className={s.bottom__block_wrapper}>
+          <li className={s.this__time}>
+            <span>21:50</span>
+          </li>
+          <li className={s.this__city}>New York</li>
+        </ul>
+      </div>
     </section>
   );
 };
