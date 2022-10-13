df1578fa9acdf1298b6b2b1bca5e1f08f1d288d6
Oleksii Maistriuk <drankel0683@gmail.com>
2022-10-13 15:37:59 +0200

    add

---
+++ src/.tokens.ts
@@ -0,0 +1,5 @@
+export const WEATHER_APP_API_KEY = 'a39cdc7ed348cdff434c1e03dfd2f0cb';
+export const WEATHER_APP_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const GEO_APP_API_KEY =
+  '13f7b1cd2dmsh805857b48dc7bbep17a950jsn7532e4c61cb4';
+export const GEO_APP_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/';
